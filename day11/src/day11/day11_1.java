05cd43611d16d1f69679a417fa7630c07add0bf9
Mockingbirs <108450080+Mockingbirs@users.noreply.github.com>
2022-07-14 17:27:26 +0900

    day 11

---
+++ day11/src/day11/day11_1.java
@@ -0,0 +1,14 @@
+package day11;
+
+import java.util.*;
+
+public class day11_1 {
+	public static void main(String[] args) {
+		ArrayList<String> pitches = new ArrayList<>();
+		pitches.add("138");
+		pitches.add("129");
+		pitches.add("142");
+		System.out.println(pitches);
+	}
+
+}
---
+++ day11/src/day11/day11_2.java
@@ -0,0 +1,18 @@
+package day11;
+
+import java.util.*;
+
+public class day11_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashMap<String, String> map = new HashMap<>();
+		map.put("people", "사람");
+			
+		map.put("baseball", "야구");
+		
+		System.out.println(map.get("people"));
+		}
+
+}
