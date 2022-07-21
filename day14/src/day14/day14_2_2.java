f4f6dcfafa6e1feb4a41578157f51f5be24ee20f
Mockingbirs <108450080+Mockingbirs@users.noreply.github.com>
2022-07-21 17:34:31 +0900

    day14

---
+++ day14/src/day14/day14_2_2.java
@@ -0,0 +1,22 @@
+package day14;
+
+public class day14_2_2 {
+	public static void main(String[] args) {
+		int arr[]= {4,2,3,6,1};
+		int i=0;
+		int t=0;
+		for(i=0;i<arr.length;i++) {
+			if(arr[i]== 5) {
+				t=1;
+				break;
+			}else{t=0;
+			}
+		}
+		if(t==1) {
+				System.out.println(arr[i]+"는"+(i+1)+"번째에 있습니다.");
+		}else {
+			System.out.println("찾는 값이 없습니다.");
+		}
+	}
+
+}
