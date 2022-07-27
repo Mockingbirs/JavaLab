df6642b5bb53a75a5fc4cdc88b52e60eaebe9fd0
Mockingbirs <108450080+Mockingbirs@users.noreply.github.com>
2022-07-27 13:07:59 +0900

    day20

--- day19/src/day19/day19_2.java
+++ day19/src/day19/day19_2.java
@@ -7,22 +7,46 @@ public class day19_2 {
 		int[] arrD = {12, 25, 36, 20, 30, 8, 42};
 		int[] arrH = new int[11];
 		
-		int i =0;
-		
-		for(i=0;i<arrD.length;i++) {
-			int k = arrD[i] % 11;
-      
-		while(true){if(arrH[k]!= 0) {
-				k = (k+1) % 11;
-			}else{arrH[k]=arrD[i];
-           break;}
-	    }
+//		int i =0;
+//		
+//		for(i=0;i<arrD.length;i++) {
+//			int k = arrD[i] % 11;
+//      
+//		while(true){if(arrH[k]!= 0) {
+//				k = (k+1) % 11;
+//			}else{arrH[k]=arrD[i];
+//           break;}
+//	    }
+//		}
+//for(int x=0;x<arrH.length;x++){    
+//    System.out.println(arrH[x]);
+//    
+//	int[] arrD = {12,25,36,20,30,8,42};
+//	int[] arrH = new int[11];
+	
+	
+	int k = 0;
+	
+	for(int i = 0; i < arrD.length; i++) {
+		k = arrD[i] % 11;
+		if(arrH[k] == 0) {
+			arrH[k] = arrD[i];
+//			System.out.println("0인 k값 : " + k);
+
+		}else if(arrH[k] != 0 ) {
+			
+			k = (k+1) % 11;
+			arrH[k] = arrD[i];
+//			System.out.println("0이 아닌 k값 : " + i);
 		}
-for(int x=0;x<arrH.length;x++){    
-    System.out.println(arrH[x]);
+		
+	}
+	for(int i = 0; i < arrH.length; i++) {
+		System.out.println(arrH[i]);
+	}
 		}
 	}
 
-}
+
 
 
---
+++ day20/src/day20/day20_1.java
@@ -0,0 +1,36 @@
+package day20;
+
+public class day20_1 {
+	public static void main(String[] args) {
+
+		int[] arrD = { 12, 13, 11, 14, 10 };
+		int i = 0;
+
+		for (int each : arrD) {             //정렬전
+			System.out.print(each+" ");	
+	 }		System.out.println();
+	 
+		for (i = 0; i < 4; i++) {
+
+			int min = i;
+
+			for (int k = i + 1; k < 5; k++) {
+				if (arrD[k] < arrD[min]) {
+					min = k;
+				}
+			}
+
+			int t = arrD[i];
+			arrD[i] = arrD[min];
+			arrD[min] = t;
+			
+			for (int each : arrD) {             //정렬후
+				System.out.print(each+" ");
+		 }		System.out.println();
+		}
+		for (int j = 0; j <arrD.length; j++) {
+			System.out.print(arrD[j]+" ");
+		}
+	}
+
+}
