c6cf3e5f092f16dcb03e63c325e6745505e3b00c
Mockingbirs <108450080+Mockingbirs@users.noreply.github.com>
2022-07-27 17:59:56 +0900

    day20

--- day19/src/day19/day19_2.java
+++ day19/src/day19/day19_2.java
@@ -1,52 +1,55 @@
 package day19;
 
 public class day19_2 {
-	
+
 	public static void main(String[] args) {
-		
-		int[] arrD = {12, 25, 36, 20, 30, 8, 42};
+
+		int[] arrD = { 12, 25, 36, 20, 30, 8, 42 };
 		int[] arrH = new int[11];
 		
-//		int i =0;
-//		
-//		for(i=0;i<arrD.length;i++) {
-//			int k = arrD[i] % 11;
-//      
-//		while(true){if(arrH[k]!= 0) {
-//				k = (k+1) % 11;
-//			}else{arrH[k]=arrD[i];
-//           break;}
-//	    }
-//		}
-//for(int x=0;x<arrH.length;x++){    
-//    System.out.println(arrH[x]);
-//    
-//	int[] arrD = {12,25,36,20,30,8,42};
-//	int[] arrH = new int[11];
-	
-	
-	int k = 0;
-	
-	for(int i = 0; i < arrD.length; i++) {
-		k = arrD[i] % 11;
-		if(arrH[k] == 0) {
-			arrH[k] = arrD[i];
-//			System.out.println("0인 k값 : " + k);
+					//해시태그 저장
+		
+		int i = 0;
 
-		}else if(arrH[k] != 0 ) {
-			
-			k = (k+1) % 11;
-			arrH[k] = arrD[i];
-//			System.out.println("0이 아닌 k값 : " + i);
+		for (i = 0; i < arrD.length; i++) {
+			int k = arrD[i] % 11;
+
+			while (true) {
+				if (arrH[k] != 0) {
+					k = (k + 1) % 11;
+				} else {
+					arrH[k] = arrD[i];
+					break;
+				}
+			}
+		}
+		for (int j = 0; j < arrH.length; j++) {
+			System.out.print(arrH[j] + " ");
+
 		}
 		
-	}
-	for(int i = 0; i < arrH.length; i++) {
-		System.out.println(arrH[i]);
-	}
+				// 해시태그 출력
+		
+		System.out.println();
+
+		int x = 8;
+		int k = x;
+		k = k % 11;
+
+		while (arrH[k] != 0) {
+			if (arrH[k] == x) {
+				System.out.println(x + "의 저장 위치는 " + k + "번째 입니다.");
+				break;
+			}
+
+			else {
+				k = (k + 1) % 11;
+			}
+
 		}
+		if (arrH[k] == 0) {
+			System.out.println(x + "값은 존재하지 않습니다.");
+		}
+
 	}
-
-
-
-
+}
