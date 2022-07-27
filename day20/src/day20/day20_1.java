53c55419b7fde3d864795dbc854bd45b9fcc5297
Mockingbirs <108450080+Mockingbirs@users.noreply.github.com>
2022-07-27 13:15:47 +0900

    day20

--- day20/src/day20/day20_1.java
+++ day20/src/day20/day20_1.java
@@ -6,9 +6,12 @@ public class day20_1 {
 		int[] arrD = { 12, 13, 11, 14, 10 };
 		int i = 0;
 
+		System.out.println("정렬전");
 		for (int each : arrD) {             //정렬전
 			System.out.print(each+" ");	
 	 }		System.out.println();
+	 
+	 System.out.println("정렬 중");
 	 
 		for (i = 0; i < 4; i++) {
 
@@ -24,10 +27,12 @@ public class day20_1 {
 			arrD[i] = arrD[min];
 			arrD[min] = t;
 			
+			
 			for (int each : arrD) {             //정렬후
 				System.out.print(each+" ");
 		 }		System.out.println();
 		}
+		System.out.println("정렬후");
 		for (int j = 0; j <arrD.length; j++) {
 			System.out.print(arrD[j]+" ");
 		}
